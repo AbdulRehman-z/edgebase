aae1ad9698a350ab0925889d9708d1b1f7b0422d
Abdul Rehman-z <yousafbhaikhan10@gmail.com>
2025-10-15 17:24:46 +0500

    integration: integrate better-auth + safe middleware(tRPC)

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,12 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
+import * as authSchema from "./schemas/auth-schema";
 
-const schema = {};
+const schema = {
+  ...authSchema,
+};
 
 const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle({ client: sql, schema });
 
-export const {} = schema;
+export const { account, session, user, verification } = schema;
