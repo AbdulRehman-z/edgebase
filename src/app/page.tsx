981746313ebdc0551f4a03fcf02435cb4b07b6a6
Abdul Rehman-z <yousafbhaikhan10@gmail.com>
2025-10-15 21:22:22 +0500

    add backgrounds jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { requireAuth } from "@/lib/auth-utlis";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
-const Home = async () => {
-  await requireAuth();
+const Home = () => {
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.getWorkFlows.queryOptions());
+  const { mutate: createWorkflow } = useMutation(trpc.createWorkflow.mutationOptions());
 
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
-      <Button>Click Me!</Button>
+      <Button onClick={() => createWorkflow()}>Create workflow</Button>
+      {JSON.stringify(data, null, 2)}
     </div>
   );
 };
--- src/db/migrations/0000_sad_wong.sql
+++ src/db/migrations/0000_sad_wong.sql
@@ -1,8 +1,8 @@
 CREATE TABLE "account" (
-	"id" text PRIMARY KEY NOT NULL,
+	"id" serial PRIMARY KEY NOT NULL,
 	"account_id" text NOT NULL,
 	"provider_id" text NOT NULL,
-	"user_id" text NOT NULL,
+	"user_id" integer NOT NULL,
 	"access_token" text,
 	"refresh_token" text,
 	"id_token" text,
@@ -15,19 +15,19 @@ CREATE TABLE "account" (
 );
 --> statement-breakpoint
 CREATE TABLE "session" (
-	"id" text PRIMARY KEY NOT NULL,
+	"id" serial PRIMARY KEY NOT NULL,
 	"expires_at" timestamp NOT NULL,
 	"token" text NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp NOT NULL,
 	"ip_address" text,
 	"user_agent" text,
-	"user_id" text NOT NULL,
+	"user_id" integer NOT NULL,
 	CONSTRAINT "session_token_unique" UNIQUE("token")
 );
 --> statement-breakpoint
 CREATE TABLE "user" (
-	"id" text PRIMARY KEY NOT NULL,
+	"id" serial PRIMARY KEY NOT NULL,
 	"name" text NOT NULL,
 	"email" text NOT NULL,
 	"email_verified" boolean DEFAULT false NOT NULL,
@@ -38,12 +38,17 @@ CREATE TABLE "user" (
 );
 --> statement-breakpoint
 CREATE TABLE "verification" (
-	"id" text PRIMARY KEY NOT NULL,
+	"id" serial PRIMARY KEY NOT NULL,
 	"identifier" text NOT NULL,
 	"value" text NOT NULL,
 	"expires_at" timestamp NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "workflow" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL
 );
 --> statement-breakpoint
 ALTER TABLE "account" ADD CONSTRAINT "account_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
